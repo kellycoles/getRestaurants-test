694e28b40421f8b5abf7a42d0548318219ab8b85
Kelly Coles <kellycoles1@gmail.com>
2019-09-02 14:56:24 -0500

    imported google maps

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 
 const getrestaurantData = (city,state,vegLevel) => {
        // I will use interplation to allow user to select city,state, and veg-level
-    return fetch(`https://www.vegguide.org/search/by-address/${city},+${state}/filter/category_id=1;veg_level=${vegLevel};?unit=mile;distance=10`)
+    return fetch(`https://www.vegguide.org/search/by-address/${city},+${state}/filter/category_id=1;category_id=5;category_id=9;veg_level=${vegLevel};?unit=mile;distance=10`)
         .then(response => response.json())
  }
 
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
- getrestaurantData("murfreesboro", "tn", "1").then((restaurants) => {
+ getrestaurantData("lincoln", "ne", "1").then((restaurants) => {
      console.log(restaurants.entries)
     for (const restaurant of restaurants.entries) {
         const restaurantHTML = createRestaurantHTML(restaurant)
