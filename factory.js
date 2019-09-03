83bf8a072d8d8609ac75a0bc17ac76db9a1196c1
Kelly Coles <kellycoles1@gmail.com>
2019-09-03 08:45:33 -0500

    test

--- factory.js
+++ factory.js
@@ -11,9 +11,9 @@ const createRestaurantHTML = (restaurant) => {
             <p>${restaurant.city}, ${restaurant.region} ${restaurant.postal_code}</p>
             <p>${restaurant.phone}</p>
             <p>${restaurant.website}</p>
+            <p>${restaurant.length}</p>
             <section>
-            <div class="mapouter"><div class="gmap_canvas"><iframe width="280" height="249" id="gmap_canvas" src="https://maps.google.com/maps?q=${restaurant.address1}%20${restaurant.city}%20${restaurant.region}&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>Google Maps Generator by <a href="https://www.embedgooglemap.net">embedgooglemap.net</a></div><style>.mapouter{position:relative;text-align:right;height:249px;width:280px;}.gmap_canvas {overflow:hidden;background:none!important;height:249px;width:280px;}</style></div>
-            </section>
+
 
         </section></br>
     `
@@ -23,4 +23,8 @@ const createRestaurantHTML = (restaurant) => {
 
 {/* <p>${restaurant.hours[0].days} ${restaurant.hours[0].hours}</p>
             <p>${restaurant.hours[1].days} ${restaurant.hours[1].hours}</p>
-            <p>${restaurant.hours[2].days} ${restaurant.hours[2].hours}</p> */}
+            <p>${restaurant.hours[2].days} ${restaurant.hours[2].hours}</p>
+
+
+                <div class="mapouter"><div class="gmap_canvas"><iframe width="280" height="249" id="gmap_canvas" src="https://maps.google.com/maps?q=${restaurant.address1}%20${restaurant.city}%20${restaurant.region}&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>Google Maps Generator by <a href="https://www.embedgooglemap.net">embedgooglemap.net</a></div><style>.mapouter{position:relative;text-align:right;height:249px;width:280px;}.gmap_canvas {overflow:hidden;background:none!important;height:249px;width:280px;}</style></div>
+            </section>*/}
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
- getrestaurantData("lincoln", "ne", "1").then((restaurants) => {
+ getrestaurantData("nashville", "tn", "1").then((restaurants) => {
      console.log(restaurants.entries)
     for (const restaurant of restaurants.entries) {
         const restaurantHTML = createRestaurantHTML(restaurant)
